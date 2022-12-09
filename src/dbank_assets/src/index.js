dd274c4ff688f734a3c283c82ebd654d4043c905
Scur <osscaaryyip@gmail.com>
2022-12-09 10:58:33 -0800

    add css, html, and js function

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,38 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async () => {
+    update()
 });
+
+document.querySelector('form').addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const button = e.target.querySelector("#submit-btn")
+    const depositAmount = parseFloat(document.getElementById('input-amount').value);
+    const withdrawAmount = parseFloat(document.getElementById('withdrawal-amount').value);
+    button.setAttribute("disabled", true)
+
+    // --------------------- Deposit -------------------------- //
+    if (document.getElementById("input-amount").value.length > 0) {
+        await dbank.depositFunds(depositAmount);
+        document.getElementById("input-amount").value = "";
+    };
+
+
+    // --------------------- Withdraw -------------------------- //   
+    if (document.getElementById("withdrawal-amount").value.length > 0) {
+        await dbank.withdrawFunds(withdrawAmount);
+        document.getElementById("withdrawal-amount").value = "";
+    };
+
+
+    // --------------------- Reset button + amount -------------------------- //
+    update()
+    button.removeAttribute("disabled");
+});
+
+
+async function update() {
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+}
